d8ce559921ddefbbe62e0fbf4a3ee9fc98a18149
Jayson Whelpley <jayson.whelpley@gmail.com>
2019-05-01 10:37:21 -0400

    Add Users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#index"
   resources :weather_records
   resources :days
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
